5db0ccb00dccbc849536967b70a05827ef6f15a4
Helton Luis <heltonlouis@gmail.com>
2022-06-28 15:30:28 -0300

    Design Responsivo

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ new Vue({
         produtos: [],
         produto: false,
         carrinho: [],
+        carrinhoAtivo: false,
         mensagemAlerta: "Item Adicionado",
         alertaAtivo: false,
     },
 
     filters: {
-      numeroPreco(valor){
-        return valor.toLocaleString(
-            "pt-BR", { style: "currency", currency: "BRL" }
-        )
-      }  
+        numeroPreco(valor) {
+            return valor.toLocaleString(
+                "pt-BR", { style: "currency", currency: "BRL" }
+            )
+        }
     },
 
     computed: {
         carrinhoTotal() {
             let total = 0;
-            if(this.carrinho.length) {
+            if (this.carrinho.length) {
                 this.carrinho.forEach(element => {
                     total += element.preco;
                 });
@@ -29,45 +30,54 @@ new Vue({
     },
 
     methods: {
-        fetchProdutos(){
+        fetchProdutos() {
             fetch("./api/produtos.json")
-            .then(resp => resp.json())
-            .then(resp => {
-                this.produtos = resp;
-            })
+                .then(resp => resp.json())
+                .then(resp => {
+                    this.produtos = resp;
+                })
         },
-        fetchItemProduto(id){
+        fetchItemProduto(id) {
             fetch(`./api/produtos/${id}/dados.json`)
-            .then(resp => resp.json())
-            .then(resp => {
-                this.produto = resp;
-            })
+                .then(resp => resp.json())
+                .then(resp => {
+                    this.produto = resp;
+                })
         },
-        abrirModal(id){
+        abrirModal(id) {
             this.fetchItemProduto(id);
             window.scrollTo({
                 top: 0,
                 behavior: "smooth"
             });
         },
-        fecharModal(event){
+        fecharModal(event) {
             if (event.target === event.currentTarget) {
                 this.produto = false;
             }
         },
-        adicionarItem(){
+        clickForaCarrinho(event) {
+            if (event.target === event.currentTarget) {
+                this.carrinhoAtivo = false;
+            }
+        },
+        adicionarItem() {
             this.produto.estoque--;
-            const {id, nome, preco } = this.produto;
+            const { id, nome, preco } = this.produto;
             this.carrinho.push({ id, nome, preco });
             this.alerta(`${nome} foi adicionado ao carrinho com sucesso!`)
         },
-        removerItem(index){
+        removerItem(index) {
             this.carrinho.splice(index, 1);
         },
         checarLocalStorage() {
-            if(window.localStorage.carrinho) {
+            if (window.localStorage.carrinho) {
                 this.carrinho = JSON.parse(window.localStorage.carrinho);
             }
+        },
+        estoque() {
+            const items = this.carrinho.filter(({ id }) => id === this.produto.id);
+            this.produto.estoque -= items.length;
         },
         alerta(mensagem) {
             this.mensagemAlerta = mensagem;
@@ -75,17 +85,32 @@ new Vue({
             setTimeout(() => {
                 this.alertaAtivo = false;
             }, 1500)
+        },
+        router() {
+            const hash = document.location.hash;
+            if (hash) {
+                this.fetchItemProduto(hash.replace("#", ""));
+            }
         }
-    }, 
+    },
 
     watch: {
+        produto() {
+            document.title = this.produto.nome || "Techno";
+            const hash = this.produto.id || "";
+            history.pushState(null, null, `#${hash}`);
+            if (this.produto) {
+                this.estoque();
+            }
+        },
         carrinho() {
             window.localStorage.carrinho = JSON.stringify(this.carrinho);
         }
     },
 
-    created(){
+    created() {
         this.fetchProdutos();
+        this.router();
         this.checarLocalStorage()
     }
 })
